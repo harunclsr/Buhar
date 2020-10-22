f55819c008352c2c9f813173eec7e58aed7ea421
Harun <harunclsr111@gmail.com>
2020-10-22 17:04:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/e_bakkal/AnaEkran.java
@@ -0,0 +1,50 @@
+package com.example.e_bakkal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.content.Intent;
+
+public class AnaEkran extends AppCompatActivity {
+
+    private ImageButton Arama;
+    private ImageButton Icecekler;
+    private ImageButton Manav;
+    private ImageButton Kisisel;
+    private ImageButton Temizlik;
+    private ImageButton Et;
+    private ImageButton Ekmek;
+    private ImageButton Atistirmalik;
+    private ImageButton Kahvaltilik;
+    private ImageButton Temel_Gida;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ana_ekran);
+
+        Arama = (ImageButton)findViewById(R.id.arama);
+        Icecekler = (ImageButton)findViewById(R.id.icecekler);
+        Manav= (ImageButton)findViewById(R.id.manav);
+        Kisisel=(ImageButton)findViewById(R.id.k_bakim);
+        Temizlik=(ImageButton)findViewById(R.id.T_urunleri);
+        Et=(ImageButton)findViewById(R.id.et);
+        Ekmek=(ImageButton)findViewById(R.id.ekmek);
+        Atistirmalik=(ImageButton)findViewById(R.id.atistirmalik);
+        Kahvaltilik=(ImageButton)findViewById(R.id.kahvaltilik);
+        Temel_Gida=(ImageButton)findViewById(R.id.temel_gida);
+
+        Icecekler.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent( AnaEkran.this, İcecekler.class);
+
+                startActivity(intent);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/e_bakkal/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.e_bakkal;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Email;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter=3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Email = (EditText)findViewById(R.id.userEmail);
+        Password = (EditText)findViewById(R.id.userSifre);
+        Info = (TextView) findViewById(R.id.tvInfo);
+        Login = (Button) findViewById(R.id.userGiris);
+        Info.setText("Giris Hakkı=3");
+        Login.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                validate (Email.getText().toString(),Password.getText().toString());
+
+            }
+        });
+    }
+
+    private void validate( String userEmail, String userSifre){
+
+        if((userEmail.equals("Admin")) && (userSifre.equals("1234"))){
+            Intent intent= new Intent(MainActivity.this, AnaEkran.class);
+
+            startActivity(intent);
+        }else{
+            counter--;
+
+            Info.setText("Giriş Hakkı="+String.valueOf(counter));
+            if(counter== 0){
+                Login.setEnabled(false);
+            }
+        }
+    }
+
+}

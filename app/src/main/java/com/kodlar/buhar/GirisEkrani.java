e219920e54d11b675792d2d064c5b9fd96f1943f
BURAK GÖÄ?ÜÅ? <burakggs44@gmail.com>
2020-11-15 16:02:23 +0300

    Burak deneme

--- app/src/main/java/com/kodlar/buhar/GirisEkrani.java
+++ app/src/main/java/com/kodlar/buhar/GirisEkrani.java
@@ -18,7 +18,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
+//Deneme
 public class GirisEkrani extends AppCompatActivity {
     private FirebaseAuth mAuth;
      EditText Email;

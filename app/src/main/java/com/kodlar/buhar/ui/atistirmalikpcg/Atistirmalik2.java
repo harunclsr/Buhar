c6223d8b6e16ba41e3bb27fbbf327c78b64b172e
BURAK GÖÄ?ÜÅ? <burakggs44@gmail.com>
2020-12-13 13:51:57 +0300

    Atıştırmalık ve Manav geri donus butonu eklendi.

--- app/src/main/java/com/kodlar/buhar/ui/atistirmalikpcg/Atistirmalik2.java
+++ app/src/main/java/com/kodlar/buhar/ui/atistirmalikpcg/Atistirmalik2.java
@@ -1,5 +1,6 @@
 package com.kodlar.buhar.ui.atistirmalikpcg;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,15 +10,19 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.kodlar.buhar.AnaEkran;
 import com.kodlar.buhar.R;
+import com.kodlar.buhar.ui.iceceklerpcg.icecekler2;
 
 import android.view.View;
+import android.widget.ImageButton;
 
 public class Atistirmalik2 extends AppCompatActivity {
-
+    private ImageButton Anasayfadonus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Anasayfadonus = (ImageButton) findViewById(R.id.AnasayfadonusAtistirmalik);
         setContentView(R.layout.activity_atistirmalik2);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
@@ -26,6 +31,15 @@ public class Atistirmalik2 extends AppCompatActivity {
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
 
+        Anasayfadonus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Atistirmalik2.this, AnaEkran.class);
+
+                startActivity(intent);
+
+            }
+        });
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/kodlar/buhar/ui/manavpckg/Manav2.java
+++ app/src/main/java/com/kodlar/buhar/ui/manavpckg/Manav2.java
@@ -1,5 +1,6 @@
 package com.kodlar.buhar.ui.manavpckg;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,15 +9,21 @@ import com.google.android.material.tabs.TabLayout;
 
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.kodlar.buhar.AnaEkran;
 import com.kodlar.buhar.R;
+import com.kodlar.buhar.ui.iceceklerpcg.icecekler2;
+
 import android.view.View;
+import android.widget.ImageButton;
 
 public class Manav2 extends AppCompatActivity {
-
+    private ImageButton Anasayfadonus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manav2);
+        Anasayfadonus = (ImageButton) findViewById(R.id.Anasayfadonusmanav);
         manavSectionsPagerAdapter manavSectionsPagerAdapter = new manavSectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(manavSectionsPagerAdapter);
@@ -24,6 +31,15 @@ public class Manav2 extends AppCompatActivity {
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
 
+        Anasayfadonus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Manav2.this, AnaEkran.class);
+
+                startActivity(intent);
+
+            }
+        });
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {

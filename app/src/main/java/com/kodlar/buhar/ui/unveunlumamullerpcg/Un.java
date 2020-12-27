6d2019346c030e74ab2d6d770467d4b266d9a978
BURAK GÖÄ?ÜÅ? <burakggs44@gmail.com>
2020-12-27 13:40:29 +0300

    Un sınıfı yapıldı.

--- app/src/main/java/com/kodlar/buhar/ui/unveunlumamullerpcg/Un.java
+++ app/src/main/java/com/kodlar/buhar/ui/unveunlumamullerpcg/Un.java
@@ -1,11 +1,127 @@
 package com.kodlar.buhar.ui.unveunlumamullerpcg;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import com.kodlar.buhar.R;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.utils.widget.ImageFilterView;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.kodlar.buhar.R;
+import com.kodlar.buhar.Urun;
+
+import com.squareup.picasso.Picasso;
 
 public class Un extends Fragment {
+    private View unView;
+    private RecyclerView myUnList;
+    private DatabaseReference UnRef,ContacsRef;
+    private FirebaseAuth mAuth;
+    private String currentUserID;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        unView= inflater.inflate(R.layout.activity_un,container,false);
+
+        myUnList=(RecyclerView) unView.findViewById(R.id.un_list);
+        myUnList.setLayoutManager(new LinearLayoutManager(getContext()));
+        mAuth= FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+
+
+        // ContacsRef = FirebaseDatabase.getInstance().getReference().child("Kampus").child("icecekler").child("Su").child(currentUserID);
+        UnRef = FirebaseDatabase.getInstance().getReference().child("Kampus").child("UnveUnluMamuller").child("Un");
+
+
+
+        return unView;
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseRecyclerOptions options=
+                new FirebaseRecyclerOptions.Builder<Urun>()
+                        .setQuery(UnRef,Urun.class)
+                        .build();
+
+        final FirebaseRecyclerAdapter<Urun, UnViewHolder> adapter = new FirebaseRecyclerAdapter<Urun, UnViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final UnViewHolder UnViewHolder, int i, @NonNull Urun urun) {
+
+                String userIDs = getRef(i).getKey();
+
+                UnRef.child(userIDs).addValueEventListener(new ValueEventListener(){
+
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot)
+                    {
+
+                        String urunfotografi = dataSnapshot.child("image").getValue(String.class);
+                        String urunadi = dataSnapshot.child("urunadi").getValue(String.class);
+                        String urunagirlik = dataSnapshot.child("urunagirlik").getValue(String.class);
+                        String urunfiyat = dataSnapshot.child("urunfiyati").getValue(String.class);
+
+                        UnViewHolder.urunadi.setText(urunadi);
+                        UnViewHolder.urunfiyat.setText(urunfiyat);
+                        UnViewHolder.urunagirlik.setText(urunagirlik);
+                        Picasso.get().load(urunfotografi.toString()).into(UnViewHolder.urunfotografi);
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Snackbar.make(unView, "HATA!!!!!!", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public UnViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+
+                View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.urun,viewGroup,false);
+                UnViewHolder viewHolder = new UnViewHolder(view);
+                return viewHolder;
+            }
+        };
+        myUnList.setAdapter(adapter);
+        adapter.startListening();
+
+
+    }
+
+    public static class UnViewHolder extends RecyclerView.ViewHolder{
+
+        TextView urunadi,urunfiyat,urunagirlik;
+        ImageFilterView urunfotografi;
+
+        public UnViewHolder(@NonNull View itemView) {
+            super(itemView);
+            urunadi= itemView.findViewById(R.id.urunAdi);
+            urunfiyat= itemView.findViewById(R.id.urunFiyat);
+            urunagirlik= itemView.findViewById(R.id.urunAgirlik);
+            urunfotografi=itemView.findViewById(R.id.urunfotografi);
+
+        }
+    }
 
 }
